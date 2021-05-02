440adfba5216974d039c229840f25bd85868ed9d
dogukannguzel <82955688+dogukannguzel@users.noreply.github.com>
2021-05-02 15:51:01 +0300

    Update NeroCustomerManager.java

--- JavaHomeWork2/Concrete/NeroCustomerManager.java
+++ JavaHomeWork2/Concrete/NeroCustomerManager.java
@@ -7,5 +7,9 @@ import Abstract.BaseCustomerManager;
 public class NeroCustomerManager extends BaseCustomerManager  {
 
 	
+	@Override
+	public void save(Customer customer) {
+	System.out.println(customer.getFirstName() + " nero saved to db. "  )
+	}
 
 }
